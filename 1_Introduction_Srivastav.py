5cdc866b2361271c80fdf5908a3a8dcebd584659
GitAnkurSri <142577723+GitAnkurSri@users.noreply.github.com>
2024-04-08 23:40:42 +0200

    First Python Script

---
+++ 1_Introduction_Srivastav.py
@@ -0,0 +1,6 @@
+# First Part (Run using "command Ä")
+# Exercises are available in youtube channel of kerstin beer (LUH).
+x = 'Hello, World!'  # puts "Hello, World!" in string x.
+y = 'I am using Python.'  # puts "I am using Python." in string y.
+print(x)  # prints the string x
+print(y)  # prints the string y
